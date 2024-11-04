ede3a20e21b59de82a2c78ad449c03cb812d3fdf
shivank04 <shivankar.rockit@gmail.com>
2024-11-04 17:59:54 +0530

    try to fix api issue

--- app.js
+++ app.js
@@ -122,3 +122,9 @@ process.on('SIGTERM', () => {
 });
 
 module.exports = app;
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
